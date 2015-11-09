da6d7b67085cc0bc3e0f50edcf1935fbf758c928
Florian Mehm <florian@mehm.net>
2015-11-09 21:07:28 +0100

    Added some comments.

--- Sources/Exercise.cpp
+++ Sources/Exercise.cpp
@@ -23,12 +23,20 @@ namespace {
 		startFrame();
 
 		clear(0, 0, 0);
-		// Add some nice transformations
+		
+		/************************************************************************/
+		/* Exercise 3 Practical Task 1                                          */
+		/* Implement camera controls to translate and rotate the mesh           */
+		/* Implement perspective projection                                     */
+		/************************************************************************/
+
 		for (int i = 0; i < mesh->numFaces; ++i) {
+			// Get the indices into the vertices array for each vertex of the mesh
 			int i1 = mesh->indices[i * 3 + 0];
 			int i2 = mesh->indices[i * 3 + 1];
 			int i3 = mesh->indices[i * 3 + 2];
 
+			// Get the x, y, z coordinates of each vertex
 			float x1 = mesh->vertices[i1 * 5 + 0];
 			float y1 = -mesh->vertices[i1 * 5 + 1];
 			float z1 = mesh->vertices[i1 * 5 + 2];
@@ -41,18 +49,21 @@ namespace {
 			float y3 = -mesh->vertices[i3 * 5 + 1];
 			float z3 = mesh->vertices[i3 * 5 + 2];
 
+			// Draw the triangle
 			drawTriangle(
 				x1 * 2000 + 600, y1 * 2000 + 600,
 				x2 * 2000 + 600, y2 * 2000 + 600,
 				x3 * 2000 + 600, y3 * 2000 + 600);
-		}
+			}
 
 		endFrame();
 	}
 
 	void keyDown(KeyCode code, wchar_t character) {
 		if (code == Key_Left) {
-			// ...
+			/************************************************************************/
+			/* Use the keyboard input to control the transformations                */
+			/************************************************************************/
 		}
 	}
 
