690ea338667bba84beb48aef08ebc969fd1d7873
Florian Mehm <florian@mehm.net>
2016-01-11 21:36:33 +0100

    Added solution for exercise 3

--- Sources/Exercise.cpp
+++ Sources/Exercise.cpp
@@ -7,20 +7,99 @@
 #include <Kore/Input/Keyboard.h>
 #include <Kore/Input/Mouse.h>
 #include <Kore/Audio/Mixer.h>
+#include <Kore/Log.h>
 #include "SimpleGraphics.h"
 #include "ObjLoader.h"
 
 using namespace Kore;
 
+#define CAMERA_ROTATION_SPEED_X 0.001
+#define CAMERA_ROTATION_SPEED_Y 0.001
+
+
 namespace {
 	double startTime;
+	float lastT = 0;
 	Mesh* mesh;
 
-	void update() {
+	const float movementSpeed = 2;
+	float zmin = -1;
+
+	float cameraX;
+	float cameraY;
+	float cameraZ;
+
+	float cameraRotX = 0;
+	float cameraRotY = 0;
+	float cameraRotZ = 0;
+
+	bool moveUp = false;
+	bool moveDown = false;
+	bool moveRight = false;
+	bool moveLeft = false;
+	bool moveForward = false;
+	bool moveBackward = false;
+
+	bool rotate = false;
+	int mousePressX, mousePressY;
+
+	void initCamera() {
+		cameraX = 0;
+		cameraY = 0;
+		cameraZ = 10;
+
+		cameraRotX = 0;
+		cameraRotY = 0;
+		cameraRotZ = 0;
+	}
+
+	void rotate3d(float &x, float &y, float &z, float rx, float ry, float rz) {
+		float d1x = cos(ry) * x + sin(ry) * z;
+		float d1y = y;
+		float d1z = cos(ry) * z - sin(ry) * x;
+		float d2x = d1x;
+		float d2y = cos(rx) * d1y - sin(rx) * d1z;
+		float d2z = cos(rx) * d1z + sin(rx) * d1y;
+		float d3x = cos(rz) * d2x - sin(rz) * d2y;
+		float d3y = cos(rz) * d2y + sin(rz) * d2x;
+		float d3z = d2z;
+
+		x = d3x;
+		y = d3y;
+		z = d3z;
+	}
+
+	void drawBunny(){
 		float t = (float)(System::time() - startTime);
-		Kore::Audio::update();
-		
-		startFrame();
+
+		float timeSinceLastFrame = t - lastT;
+		lastT = t;
+
+		//update camera:
+		float cameraMovementX = 0;
+		float cameraMovementY = 0;
+		float cameraMovementZ = 0;
+
+		if (moveUp)
+			cameraMovementY -= timeSinceLastFrame * movementSpeed;
+		if (moveDown)
+			cameraMovementY += timeSinceLastFrame * movementSpeed;
+		if (moveLeft)
+			cameraMovementX -= timeSinceLastFrame * movementSpeed;
+		if (moveRight)
+			cameraMovementX += timeSinceLastFrame * movementSpeed;
+		if (moveForward)
+			cameraMovementZ -= timeSinceLastFrame * movementSpeed;
+		if (moveBackward)
+			cameraMovementZ += timeSinceLastFrame * movementSpeed;
+
+		//rotate direction according to current rotation
+		rotate3d(cameraMovementX, cameraMovementY, cameraMovementZ, -cameraRotX, 0, -cameraRotZ);
+		rotate3d(cameraMovementX, cameraMovementY, cameraMovementZ, 0, -cameraRotY, -cameraRotZ);
+
+		cameraX += cameraMovementX;
+		cameraY += cameraMovementY;
+		cameraZ += cameraMovementZ;
 
 		clear(0, 0, 0);
 		
@@ -31,12 +110,10 @@ namespace {
 		/************************************************************************/
 
 		for (int i = 0; i < mesh->numFaces; ++i) {
-			// Get the indices into the vertices array for each vertex of the mesh
 			int i1 = mesh->indices[i * 3 + 0];
 			int i2 = mesh->indices[i * 3 + 1];
 			int i3 = mesh->indices[i * 3 + 2];
 
-			// Get the x, y, z coordinates of each vertex
 			float x1 = mesh->vertices[i1 * 5 + 0];
 			float y1 = -mesh->vertices[i1 * 5 + 1];
 			float z1 = mesh->vertices[i1 * 5 + 2];
@@ -49,55 +126,150 @@ namespace {
 			float y3 = -mesh->vertices[i3 * 5 + 1];
 			float z3 = mesh->vertices[i3 * 5 + 2];
 
-			// Draw the triangle
+			//move vectors
+			x1 -= cameraX;
+			y1 -= cameraY;
+			z1 -= cameraZ;
+			x2 -= cameraX;
+			y2 -= cameraY;
+			z2 -= cameraZ;
+			x3 -= cameraX;
+			y3 -= cameraY;
+			z3 -= cameraZ;
+
+			//camera rotation
+			rotate3d(x1, y1, z1, cameraRotX, cameraRotY, cameraRotZ);
+			rotate3d(x2, y2, z2, cameraRotX, cameraRotY, cameraRotZ);
+			rotate3d(x3, y3, z3, cameraRotX, cameraRotY, cameraRotZ);
+
+			if (z1 > zmin || z2 > zmin || z3 > zmin)
+				continue;
+
+			//perspective transform:
+			x1 = (zmin / z1) * x1;
+			y1 = (zmin / z1) * y1;
+			x2 = (zmin / z2) * x2;
+			y2 = (zmin / z2) * y2;
+			x3 = (zmin / z3) * x3;
+			y3 = (zmin / z3) * y3;
+
 			drawTriangle(
-				x1 * 2000 + 600, y1 * 2000 + 600,
-				x2 * 2000 + 600, y2 * 2000 + 600,
-				x3 * 2000 + 600, y3 * 2000 + 600);
-			}
+				x1 * height + width / 2, y1 * height + height / 2,
+				x2 * height + width / 2, y2 * height + height / 2,
+				x3 * height + width / 2, y3 * height + height / 2);
+		}
+	}
+
+
+
+	void update() {
+		Kore::Audio::update();
+		startFrame();
+
+		drawBunny();
 
 		endFrame();
 	}
 
 	void keyDown(KeyCode code, wchar_t character) {
-		if (code == Key_Left) {
-			/************************************************************************/
-			/* Use the keyboard input to control the transformations                */
-			/************************************************************************/
+		switch (code)
+		{
+		case Key_Left:
+		case Key_A:
+			moveLeft = true;
+			break;
+		case Key_Right:
+		case Key_D:
+			moveRight = true;
+			break;
+		case Key_Up:
+			moveUp = true;
+			break;
+		case Key_Down:
+			moveDown = true;
+			break;
+		case Key_W:
+			moveForward = true;
+			break;
+		case Key_S:
+			moveBackward = true;
+			break;
+		case Key_R:
+			initCamera();
+			break;
+		case Key_L:
+			Kore::log(Kore::LogLevel::Info, "Position: (%.2f, %.2f, %.2f) - Rotation: (%.2f, %.2f, %.2f)\n", cameraX, cameraY, cameraZ, cameraRotX, cameraRotY, cameraRotZ);
+			break;
+		default:
+			break;
 		}
 	}
 
 	void keyUp(KeyCode code, wchar_t character) {
-		if (code == Key_Left) {
-			// ...
+		switch (code)
+		{
+		case Key_Left:
+		case Key_A:
+			moveLeft = false;
+			break;
+		case Key_Right:
+		case Key_D:
+			moveRight = false;
+			break;
+		case Key_Up:
+			moveUp = false;
+			break;
+		case Key_Down:
+			moveDown = false;
+			break;
+		case Key_W:
+			moveForward = false;
+			break;
+		case Key_S:
+			moveBackward = false;
+			break;
+		default:
+			break;
 		}
 	}
 
 	void mouseMove(int x, int y, int movementX, int movementY) {
-
+		if (rotate) {
+			cameraRotX += (float)((mousePressY - y) * CAMERA_ROTATION_SPEED_X);
+			cameraRotY -= (float)((mousePressX - x) * CAMERA_ROTATION_SPEED_Y);
+			mousePressX = x;
+			mousePressY = y;
+		}
 	}
-	
-	void mousePress(int button, int x, int y) {
 
+	void mousePress(int button, int x, int y) {
+		rotate = true;
+		mousePressX = x;
+		mousePressY = y;
 	}
 
 	void mouseRelease(int button, int x, int y) {
-
+		rotate = false;
 	}
 }
 
 int kore(int argc, char** argv) {
-	Application* app = new Application(argc, argv, width, height, 0, false, "Exercise3");
-	
+	Application* app = new Application(argc, argv, width, height, 0, false, "Solution3");
+
 	initGraphics();
 	app->setCallback(update);
 
 	startTime = System::time();
 	Kore::Mixer::init();
 	Kore::Audio::init();
-	//Kore::Mixer::play(new SoundStream("back.ogg", true));
 
 	mesh = loadObj("bunny.obj");
+
+	for (int i = 0; i < mesh->numVertices; i++) {
+		mesh->vertices[i * 5 + 0] *= 10;
+		mesh->vertices[i * 5 + 1] *= 10;
+		mesh->vertices[i * 5 + 2] *= 10;
+	}
 
 	Keyboard::the()->KeyDown = keyDown;
 	Keyboard::the()->KeyUp = keyUp;
@@ -105,9 +277,11 @@ int kore(int argc, char** argv) {
 	Mouse::the()->Press = mousePress;
 	Mouse::the()->Release = mouseRelease;
 
+	initCamera();
+
 	app->start();
 
 	delete app;
-	
+
 	return 0;
 }
